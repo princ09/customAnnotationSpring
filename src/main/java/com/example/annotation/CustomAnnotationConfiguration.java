108e6dca5503199a6f2fd84e9c7c9c427acf9109
Rajkumar Gupta <rajkumargupta9615@gmail.com>
2020-09-02 18:18:58 +0530

    initial commit

---
+++ src/main/java/com/example/annotation/CustomAnnotationConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.baeldung.customannotation")
+public class CustomAnnotationConfiguration {
+}
